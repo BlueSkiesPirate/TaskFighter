600e143f7d569a520e3f099d5133ec3e81169fbe
BlueSkiesPirate <sebastianreyna128@gmail.com>
2024-03-31 11:21:57 -0400

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ function App() {
     <>
       <div id="top_nav">
         <div id="greeting_conatiner">
-          Hello Sebastian, today is <strong>Feb 23, 2024</strong>
+          Hello <br />
+          Sebastian, today is <strong>Feb 23, 2024</strong>
         </div>
         <div id="user_container">
           <img
@@ -20,12 +21,42 @@ function App() {
         </div>
       </div>
 
-      <div id="main_dashboard"></div>
+      <div id="main_dashboard">
+        <div id="dash">
+          <div id="dash_left"></div>
+          <div id="dash_middle">
+            <div className="dash_middle_container">Health</div>
+            <div className="dash_middle_container">coins</div>
+            <div className="dash_middle_container" id="lvl_text">
+              Lvl
+            </div>
+          </div>
+          <div id="dash_right">
+            <div className="dash_right_container">
+              <div id="health"></div>
+            </div>
+            <div className="dash_right_container" id="coin_number">
+              120
+            </div>
+            <div className="dash_right_container" id="lvl_text">
+              10
+            </div>
+          </div>
+        </div>
+      </div>
 
-      <div id="daily_tasks"></div>
+      <div id="daily_tasks">
+        <div id="daily_tasks_container"></div>
+      </div>
 
-      <div id="other_widgets"></div>
-      <div id="projects_container"></div>
+      <div id="other_widgets">
+        <div className="other_widget" id="side_quests"></div>
+        <div className="other_widget" id="battle"></div>
+      </div>
+
+      <div id="projects_container">
+        <div id="project_container"></div>
+      </div>
     </>
   );
 }
